ffe924259781360b7be97a9b96d50be8196d96d0
HJ Park <b2aconnn@gmail.com>
2025-06-12 16:23:39 +0900

    feat(springboot-and-jpa-part1 - section 6): 주문, 주문상품 엔티티 개발

--- springbootAndJpaPart1/jpashop/src/main/java/jpabook/jpashop/domain/Order.java
+++ springbootAndJpaPart1/jpashop/src/main/java/jpabook/jpashop/domain/Order.java
@@ -8,6 +8,10 @@ import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
+import static jpabook.jpashop.domain.DeliveryStatus.COMP;
+import static jpabook.jpashop.domain.OrderStatus.CANCEL;
+import static jpabook.jpashop.domain.OrderStatus.ORDER;
+
 @Entity
 @Table(name = "orders")
 @Getter
@@ -48,5 +52,37 @@ public class Order {
     public void addDelivery(Delivery delivery) {
         this.delivery = delivery;
         delivery.setOrder(this);
+    }
+
+    // 생성 메서드
+    public static Order createOrder(Member member, Delivery delivery, OrderItem... orderItems) {
+        Order order = new Order();
+        order.setMember(member);
+        order.setDelivery(delivery);
+        for (OrderItem orderItem : orderItems) {
+            order.addOrderItem(orderItem);
+        }
+        order.setStatus(ORDER);
+        order.setOrderDate(LocalDateTime.now());
+        return order;
+    }
+
+    // 주문 취소
+    public void cancel() {
+        if (delivery.getStatus().equals(COMP)) {
+            throw new IllegalStateException("이미 배송 완료된 상품은 취소가 불가능합니다.");
+        }
+
+        this.status = CANCEL;
+        for (OrderItem orderItem : orderItems) {
+            orderItem.cancel();
+        }
+    }
+
+    // 조회 로직
+    public int getTotalPrice() {
+        return orderItems.stream()
+                .mapToInt(OrderItem::getTotalPrice)
+                .sum();
     }
 }
