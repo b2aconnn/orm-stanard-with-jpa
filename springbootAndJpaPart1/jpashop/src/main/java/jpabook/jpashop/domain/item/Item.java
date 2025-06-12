32c4bf5d400c42fe943e31c7bf4631870d006f15
HJ Park <b2aconnn@gmail.com>
2025-06-12 15:33:51 +0900

    feat(springboot-and-jpa-part1 - section 6): 상품 엔티티 개발(비즈니스 로직 추가)

--- springbootAndJpaPart1/jpashop/src/main/java/jpabook/jpashop/domain/item/Item.java
+++ springbootAndJpaPart1/jpashop/src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -1,6 +1,7 @@
 package jpabook.jpashop.domain.item;
 
 import jakarta.persistence.*;
+import jpabook.jpashop.exception.NotEnoughStockException;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -16,4 +17,17 @@ public abstract class Item {
     private String name;
     private int price;
     private int stockQuantity;
+
+    //==비즈니스 로직==//
+    public void addStock(int quantity) {
+        this.stockQuantity += quantity;
+    }
+
+    public void removeStock(int quantity) {
+        int restStock = this.stockQuantity - quantity;
+        if (restStock < 0) {
+            throw new NotEnoughStockException("need more stock");
+        }
+        this.stockQuantity = restStock;
+    }
 }
