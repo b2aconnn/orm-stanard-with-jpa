413714f22e341ca6c57efb025f259dd576322e92
HJ Park <b2aconnn@gmail.com>
2025-06-14 23:21:26 +0900

    feat(springboot-and-jpa-part2 - section 4): 간단한 주문 조회 V4: JPA에서 DTO로 바로 조회

--- springbootAndJpaPart1/jpashop/src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
+++ springbootAndJpaPart1/jpashop/src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
@@ -1,17 +1,17 @@
 package jpabook.jpashop.api;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import jpabook.jpashop.domain.Address;
 import jpabook.jpashop.domain.Order;
 import jpabook.jpashop.domain.OrderStatus;
 import jpabook.jpashop.repository.*;
+import jpabook.jpashop.repository.order.simplequery.OrderSimpleQueryDto;
+import jpabook.jpashop.repository.order.simplequery.OrderSimpleQueryRepository;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDateTime;
-import java.util.ArrayList;
 import java.util.List;
 
 import static java.util.stream.Collectors.toList;
@@ -29,7 +29,7 @@ import static java.util.stream.Collectors.toList;
 public class OrderSimpleApiController {
 
     private final OrderRepository orderRepository;
-//    private final OrderSimpleQueryRepository orderSimpleQueryRepository; //의존관계 주입
+    private final OrderSimpleQueryRepository orderSimpleQueryRepository; //의존관계 주입
 
     /**
      * V1. 엔티티 직접 노출
@@ -73,16 +73,15 @@ public class OrderSimpleApiController {
                 .map(o -> new SimpleOrderDto(o))
                 .collect(toList());
     }
-//
-//    @GetMapping("/api/v4/simple-orders")
-//    public List<OrderSimpleQueryDto> ordersV4() {
-//        return orderSimpleQueryRepository.findOrderDtos();
-//    }
-//
-//
+
+    @GetMapping("/api/v4/simple-orders")
+    public List<OrderSimpleQueryDto> ordersV4() {
+        return orderSimpleQueryRepository.findOrderDtos();
+    }
+
+
     @Data
     static class SimpleOrderDto {
-
         private Long orderId;
         private String name;
         private LocalDateTime orderDate; //주문시간
