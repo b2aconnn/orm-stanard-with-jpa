d3f361b997b097875e433550623b6b4a0dc2bf98
HJ Park <b2aconnn@gmail.com>
2025-06-14 22:52:05 +0900

    feat(springboot-and-jpa-part2 - section 4): 간단한 주문 조회 V3: 엔티티를 DTO로 변환 - 페치 조인 최적화

--- springbootAndJpaPart1/jpashop/src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
+++ springbootAndJpaPart1/jpashop/src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 
 import static java.util.stream.Collectors.toList;
@@ -59,20 +60,19 @@ public class OrderSimpleApiController {
 
         return result;
     }
-//
-//    /**
-//     * V3. 엔티티를 조회해서 DTO로 변환(fetch join 사용O)
-//     * - fetch join으로 쿼리 1번 호출
-//     * 참고: fetch join에 대한 자세한 내용은 JPA 기본편 참고(정말 중요함)
-//     */
-//    @GetMapping("/api/v3/simple-orders")
-//    public List<SimpleOrderDto> ordersV3() {
-//        List<Order> orders = orderRepository.findAllWithMemberDelivery();
-//        List<SimpleOrderDto> result = orders.stream()
-//                .map(o -> new SimpleOrderDto(o))
-//                .collect(toList());
-//        return result;
-//    }
+
+    /**
+     * V3. 엔티티를 조회해서 DTO로 변환(fetch join 사용O)
+     * - fetch join으로 쿼리 1번 호출
+     * 참고: fetch join에 대한 자세한 내용은 JPA 기본편 참고(정말 중요함)
+     */
+    @GetMapping("/api/v3/simple-orders")
+    public List<SimpleOrderDto> ordersV3() {
+        List<Order> orders = orderRepository.findAllWithMemberDelivery();
+        return orders.stream()
+                .map(o -> new SimpleOrderDto(o))
+                .collect(toList());
+    }
 //
 //    @GetMapping("/api/v4/simple-orders")
 //    public List<OrderSimpleQueryDto> ordersV4() {
