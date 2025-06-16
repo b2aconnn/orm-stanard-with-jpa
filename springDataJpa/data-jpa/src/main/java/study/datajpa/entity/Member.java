454814b9a05783a9dd93a8c2c4382dbecf75af21
HJ Park <b2aconnn@gmail.com>
2025-06-16 13:05:28 +0900

    feat(spring-data-jpa - section 3): 예제 도메인 모델과 동작확인

--- springDataJpa/data-jpa/src/main/java/study/datajpa/entity/Member.java
+++ springDataJpa/data-jpa/src/main/java/study/datajpa/entity/Member.java
@@ -1,23 +1,30 @@
 package study.datajpa.entity;
 
 import jakarta.persistence.*;
-import lombok.AccessLevel;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Entity
 @Getter @Setter
+@ToString(of = {"id", "username", "age"})
 public class Member {
     @Id
     @GeneratedValue
-//    @Column(name = "member_id")
+    @Column(name = "member_id")
     private Long id;
-
     private String username;
+    private int age;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "team_id")
+    private Team team;
 
     public Member(String username) {
         this.username = username;
+    }
+
+    public void changeTeam(Team team) {
+        this.team = team;
+        team.getMembers().add(this);
     }
 }
